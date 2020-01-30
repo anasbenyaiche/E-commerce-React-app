a6eb9232126380a76f301cf95d882c589cc5e0aa
anasbenyaiche <anas.benyaiche@yahoo.fr>
2020-01-30 12:14:29 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,61 @@
 import React from 'react';
-import Home from './pages/homepage/homepage.component'
+import HomePage from './pages/homepage/homepage.component';
+import {Route,Switch} from 'react-router-dom';
 import './App.css';
 
+const HatsPage = () => {
+  return <div><h1>HATS</h1></div>
+}
+const SneakersPage = () => {
+  return <div><h1>Sneakers Page</h1></div>
+}
+const JacketsPage = () => {
+  return <div><h1>Jackets Page</h1></div>
+}
+const MensPage = () => {
+  return <div><h1>Men's Page</h1></div>
+}
+const WomensPage = () => {
+  return <div><h1>Women's Page</h1></div>
+}
+let route=[
+    {
+      id:0,
+      path:'/',
+      component:HomePage,
+    },
+    {
+      id:1,
+      path:'/hats',
+      component:HatsPage,
+    },
+    {
+      id:2,
+      path:'/sneakers',
+      component:SneakersPage,
+    },
+    {
+      id:3,
+      path:'/jackets',
+      component:JacketsPage,
+    },
+    {
+      id:4,
+      path:'/womens',
+      component:WomensPage,
+    },
+    {
+      id:5,
+      path:'/mens',
+      component:MensPage,
+    }
+]
 function App() {
   return (
     <div >
-      <Home/>
-
+      <Switch>
+    {route.map(e=><Route path={e.path} exact component={e.component} />)}
+    </Switch>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,56 +1,70 @@
-import React from 'react'
-import './directory.style.scss'
-import MenuItem from '../menu-item/menu-item.component'
+import React from "react";
+import "./directory.style.scss";
+import MenuItem from "../menu-item/menu-item.component";
 
-class Directory extends React.Component{
-    constructor(){
-        super();
-        this.state = {
-            sections:
-            [
-                {
-                    title:"Hats",
-                    imageURL:"https://images.unsplash.com/photo-1533055640609-24b498dfd74c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80",
-                    id:1
-            
-                },  
-                {
-                    title:"Jackets",
-                    imageURL:"https://images.unsplash.com/photo-1509539662397-116cb90542f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-                    id:2
-                },
-                {
-                    title:"Sneakers",
-                    imageURL:"https://images.unsplash.com/photo-1524010349062-860def6649c3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
-                    id:3
-                },               
-                {
-                    title:"Women",
-                    imageURL:"https://images.unsplash.com/photo-1495516372021-9c815fa7e668?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
-                    id:4,
-                    size:'large'
-                },
-                {
-                    title:"Man",
-                    imageURL:"https://images.unsplash.com/photo-1574933408994-6f0539cab5df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1023&q=80",
-                    id:5,
-                    size:'large'
-                }   
-            ]
+class Directory extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      sections: [
+        {
+          title: "Hats",
+          linkURL: "hats",
+          imageURL:
+            "https://images.unsplash.com/photo-1533055640609-24b498dfd74c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80",
+          id: 1
+        },
+        {
+          title: "Jackets",
+          linkURL: "jackets",
+          imageURL:
+            "https://images.unsplash.com/photo-1509539662397-116cb90542f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+          id: 2
+        },
+        {
+          title: "Sneakers",
+          linkURL: "sneakers",
+          imageURL:
+            "https://images.unsplash.com/photo-1524010349062-860def6649c3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+          id: 3
+        },
+        {
+          title: "Women",
+          linkURL: "womens",
+          imageURL:
+            "https://images.unsplash.com/photo-1495516372021-9c815fa7e668?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+          id: 4,
+          size: "large"
+        },
+        {
+          title: "Men",
+          linkURL: "mens",
+          imageURL:
+            "https://images.unsplash.com/photo-1574933408994-6f0539cab5df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1023&q=80",
+          id: 5,
+          size: "large"
         }
-    }
-    render(){
-        return(
-            <div className="directory-menu">
-                
-                {this.state.sections.map(({title,size, imageURL, id})=>(
-                    <MenuItem 
-                    key={id}
-                    title={title} 
-                    imageURL={imageURL}  size={size}/>
-                ))}
-            </div>
-        )
-    }
+      ]
+    };
+  }
+  render() {
+    return (
+      <div className="directory-menu">
+        {/* ...otherSctionProps is similar to destructing the object 
+        {this.state.sections.map(({ title, size, imageURL, linkURL, id }) => (
+          <MenuItem
+            key={id}
+            title={title}
+            imageURL={imageURL}
+            linkURL={linkURL}
+            size={size}
+          />
+        ))} */}
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem {...otherSectionProps} />
+        ))}
+      </div>
+    );
+  }
 }
 export default Directory;
