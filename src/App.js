bc4571374538f7231c609c9d7aa3305867234fec
anasbenyaiche <anas.benyaiche@yahoo.fr>
2020-02-05 11:50:46 +0100

    creating the header

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HomePage from './pages/homepage/homepage.component';
 import {Route,Switch} from 'react-router-dom';
 import './App.css';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 
 const HatsPage = () => {
@@ -61,7 +62,7 @@ let route=[
 function App() {
   return (
     <div >
-
+      <Header/>
       <Switch>
     {route.map(e=><Route path={e.path} exact component={e.component} />)}
     </Switch>
