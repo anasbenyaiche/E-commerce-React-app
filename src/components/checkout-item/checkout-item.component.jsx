537ae400a40abe8aa683e3d637f8a19495086cc1
anasbenyaiche <anas.benyaiche@yahoo.fr>
2020-03-29 20:18:29 +0100

    checkout page nearly done

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import "./checkout-item.style.scss";
 const CheckoutItem = ({ cartItem: { imageURL, name, price, quantity } }) => {
-
-    return(
-  <div className="checkout-item">
-    <div className="image-container">
-      <img src={imageURL} alt="item" />
+  return (
+    <div className="checkout-item">
+      <div className="image-container">
+        <img src={imageURL} alt="item" />
+      </div>
+      <span className="name">{name}</span>
+      <span className="quantity">{quantity}</span>
+      <span className="price">{price}</span>
+      <div className="remove-button">
+        <span role="img" aria-label="cross" onClick={() => {}}>
+          ❌
+        </span>
+      </div>
     </div>
-    <span className="name">{name}</span>
-    <span className="quantity">{quantity}</span>
-    <span className="price">{price}</span>
-      <div className="remove-button"><span role="img" aria-label="cross" onClick={() => {}}>❌</span></div>  
-  </div>
-)};
+  );
+};
 export default CheckoutItem;
