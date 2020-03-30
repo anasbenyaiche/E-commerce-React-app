37a899fef102cc7c3dd1d64ea68c4c5e687e5412
anasbenyaiche <anas.benyaiche@yahoo.fr>
2020-03-30 15:23:50 +0100

    add item and remove item functionnality

--- src/redux/cart/cart.reducers.js
+++ src/redux/cart/cart.reducers.js
@@ -1,13 +1,12 @@
- import CartActionTypes from './cart.types'
-import CartActionsTypes from './cart.types';
-import { addItemToCart } from './cart.utils';
- 
+import CartActionTypes from "./cart.types";
+import CartActionsTypes from "./cart.types";
+import { addItemToCart, removeItemFromCart } from "./cart.utils";
+
 const INITITAL_STATE = {
   hidden: true,
-  cartItems:[],
-
+  cartItems: []
 };
-console.log(INITITAL_STATE.cartItems)
+console.log(INITITAL_STATE.cartItems);
 
 const cartRedcuer = (state = INITITAL_STATE, action) => {
   switch (action.type) {
@@ -17,14 +16,25 @@ const cartRedcuer = (state = INITITAL_STATE, action) => {
         hidden: !state.hidden
       };
     case CartActionsTypes.ADD_ITEM:
-        
-        return {
-            ...state,
-            cartItems: addItemToCart(state.cartItems, action.payload)
-        }
+      return {
+        ...state,
+        cartItems: addItemToCart(state.cartItems, action.payload)
+      };
+    case CartActionsTypes.ClEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          cartItem => cartItem.id !== action.payload.id
+        )
+      };
+    case CartActionsTypes.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems : removeItemFromCart(state.cartItems, action.payload)
+      }
 
     default:
       return state;
   }
 };
-export default cartRedcuer
+export default cartRedcuer;
